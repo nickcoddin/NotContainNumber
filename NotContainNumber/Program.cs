10f21f341766677eb53b727e2d812de3958832c6
Nick <nikoloz.de@gmail.com>
2023-09-28 16:57:04 +0400

    Add project files.

---
+++ NotContainNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 9, 15, 7, 4, 3, 5, 1, 2, 7 };
+        int result = NotContains(array);
+        Console.WriteLine(result);
+    }
+
+    public static int NotContains(int[] array)
+    {
+        int minNumber = 1;
+
+        while (array.Contains(minNumber))
+        {
+            minNumber++;
+        }
+
+        return minNumber;
+    }
+}
